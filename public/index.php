73be0f669ae50abe77e262ad90afb25e15feed49
Juan Carielo <juancarielo@gmail.com>
2018-06-05 16:26:19 -0300

    Created a new structure

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use App\Router;
+
+$router = new Router();
+
+$router->on('GET', 'path/to/action', function () {
+    return 'this is a hero return';
+});
+
+$router->post('/(\w+)/(\w+)/(\w+)', function ($module, $class, $method) {
+    var_dump('here custom');
+    var_dump([$module, $class, $method]);
+});
+
+$router->get('/view/(\w+)', function ($view) {
+    ob_start();
+    require dirname(__DIR__) . "/src/View/{$view}.php";
+    $html = ob_get_contents();
+    ob_end_clean();
+    return $html;
+});
+
+$router->get('/(.*)', function ($uri) {
+    var_dump('here final');
+    var_dump($uri);
+});
+
+echo $router($router->method(), $router->uri());
